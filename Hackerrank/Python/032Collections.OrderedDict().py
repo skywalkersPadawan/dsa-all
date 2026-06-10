6b92b24dc166ca63c498c22a584f38a459f73f32
skywalkersPadawan <pavandheeraj.professional9590@gmail.com>
2026-06-10 21:06:52 +0530

    Update 032Collections.OrderedDict().py

--- Hackerrank/Python/032Collections.OrderedDict().py
+++ Hackerrank/Python/032Collections.OrderedDict().py
@@ -3,3 +3,14 @@
 from collections import OrderedDict
 
 n = int(input())
+items = OrderedDict()
+
+for _ in range(n):
+    item_name, net_price = input().split()
+    items[item_name] += int(net_price)
+
+for item_name, net_price in items.items():
+    print(item_name, net_price)
+
+
+# all the test cases failed study the lib correctly and then solve the problem
