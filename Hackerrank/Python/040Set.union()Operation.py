81c94dc5dc0a3d214eb5026e3e1e6926a16d2aed
skywalkersPadawan <pavandheeraj.professional9590@gmail.com>
2026-06-17 08:35:35 +0530

    Create 040Set.union()Operation.py

---
+++ Hackerrank/Python/040Set.union()Operation.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+n_set = set(map(int, input().split()))
+b = int(input())
+b_set = set(map(int, input().split()))
+
+union_set = n_set.union(b_set)
+print(len(union_set))
