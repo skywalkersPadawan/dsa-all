55629fc45f7062a1bedf94393fe3069801f0c824
skywalkersPadawan <pavandheeraj.professional9590@gmail.com>
2026-06-19 22:52:36 +0530

    Create 042ModDivmod.py

---
+++ Hackerrank/Python/042ModDivmod.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# from math import divmod
+# divmod is a built-in function that returns the quotient and remainder of the division of two number divmod() is a built-in function so it is available automatically without needing to import it
+
+a = int(input())
+b = int(input())
+print(a // b)
+print(a % b)
+print(divmod(a, b))
