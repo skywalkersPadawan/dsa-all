40da68190d87ee1b2315a46192ec214a5a15ff37
skywalkersPadawan <pavandheeraj.professional9590@gmail.com>
2026-06-25 23:55:56 +0530

    LC(Blind75): (python-easy) 572.SubtreeofAnotherTree
    - Compare the entire subtree using isSameTree(root, subRoot)
    
    - Continue traversing left and right when a candidate node does not match
    
    - Fixes cases where multiple nodes share the same value

---
+++ LeetCode/Blind75/python/572.SubtreeofAnotherTree.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root and not subRoot:
+            return True
+        if not root and subRoot:
+            return False
+        if root and not subRoot:
+            return False
+        if root.val == subRoot.val:
+            if self.isSameTree(root, subRoot):
+                return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root and not subRoot:
+            return True
+        if not root and subRoot:
+            return False
+        if root and not subRoot:
+            return False
+        if root.val == subRoot.val:
+            return self.isSameTree(root.left, subRoot.left) and self.isSameTree(root.right, subRoot.right)
+        return False
+
+
+# this might work but need to test a couple of times searching for subtree should continue because somewhere else in the tree might be the subtree
+
+# 169 / 185 testcases passed, need to fix it; need to learn more examples of trees traversal
+
+# missed the obvious starting point of treating matching values as the starting point of the subtree
