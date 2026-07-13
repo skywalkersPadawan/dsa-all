3d1fb59bd2f54a9fb2df8f2d216c2e7f1bc6b0d1
skywalkersPadawan <pavandheeraj.professional9590@gmail.com>
2026-07-13 22:49:25 +0530

    Update 032Collections.OrderedDict().py

--- Hackerrank/Python/032Collections.OrderedDict().py
+++ Hackerrank/Python/032Collections.OrderedDict().py
@@ -1,16 +1,16 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-from collections import OrderedDict
+# from collections import OrderedDict
 
-n = int(input())
-items = OrderedDict()
+# n = int(input())
+# items = OrderedDict()
 
-for _ in range(n):
-    item_name, net_price = input().split()
-    items[item_name] += int(net_price)
+# for _ in range(n):
+#     item_name, net_price = input().split()
+#     items[item_name] += int(net_price)
 
-for item_name, net_price in items.items():
-    print(item_name, net_price)
+# for item_name, net_price in items.items():
+#     print(item_name, net_price)
 
 
 # all test cases failed study the library correctly and then solve the problem
@@ -24,3 +24,20 @@ will return ['BANANA', 'FRIES', '12']
 
 that is the issue python cannot unpack 3 values into 2 variables New items will raise KeyError because they are not initialized in the OrderedDict. Also, item names may contain spaces, and that is why this will not work
 '''
+
+
+# from the discussion above the solution will be as follows
+
+from collections import OrderedDict
+
+N = int(input())
+items = OrderedDict()
+
+for _ in range(N):
+    parts = input().split()
+    net_price = int(parts[-1])
+    item_name = ' '.join(parts[:-1])
+    items[item_name] = items.get(item_name, 0) + net_price
+
+for item_name, net_price in items.items():
+    print(item_name, net_price)
