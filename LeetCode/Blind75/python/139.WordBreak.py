744bbb6c20c8a1c9e4e3a7a2074388b5e76d5047
skywalkersPadawan <pavandheeraj.professional9590@gmail.com>
2026-08-18 00:02:29 +0530

    add incomplete solutions work on this later

---
+++ LeetCode/Blind75/python/139.WordBreak.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        pass
+
+    # not able to solve or form a solution test cases don't make sense use dp approach as optimal approach from chatgpt and solve similar problems to see if the pattern works
+    # solve all high acceptance rate medium problems next (greater than 60% — easier and straightforward usually)
