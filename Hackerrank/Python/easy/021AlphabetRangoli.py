decd917053afc9b636fcea32f9f6c5da67e1df07
skywalkersPadawan <pavandheeraj.professional9590@gmail.com>
2026-08-26 18:16:45 +0530

    HR(python3-easy): solved last problem AlphabetRangoli
    finished HackerRank Python Challenges need to revise pattern generating problems

--- Hackerrank/Python/easy/021AlphabetRangoli.py
+++ Hackerrank/Python/easy/021AlphabetRangoli.py
@@ -1,9 +1,18 @@
 def print_rangoli(size):
-    # your code goes here
-    pass
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    lines = []
+
+    # Upper half (including middle)
+    for i in range(size):
+        s = "-".join(alpha[size - 1 : i : -1] + alpha[i:size])
+        width = 4 * size - 3
+        lines.append(s.center(width, "-"))
+
+    # Print full rangoli
+    print("\n".join(lines[:0:-1] + lines))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     n = int(input())
     print_rangoli(n)
 
